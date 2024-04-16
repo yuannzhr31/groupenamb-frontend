88763a4131ca057887cdbfe5d33550e4b8170ff8
dzikri028 <yuandazahraaulia@gmail.com>
2024-04-17 03:08:13 +0700

    landing page beres

--- js/main.js
+++ js/main.js
@@ -174,39 +174,23 @@
     },
   });
 
+  // kedai
+  // Script untuk mengubah warna ikon love saat diklik
+  const heartIcons = document.querySelectorAll(".heart-icon");
+
+  heartIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      icon.classList.toggle("far"); // toggle class 'far' untuk mengubah warna ikon
+      icon.classList.toggle("fas"); // toggle class 'fas' untuk mengubah warna ikon
+    });
+  });
+
   // Informasi carousel
   $(".info-carousel").owlCarousel({
     autoplay: true,
     smartSpeed: 1000,
-    center: true,
-    dots: true,
-    loop: true,
-    margin: 25,
-    nav: true,
-    navText: ['<i class="bi bi-arrow-left"></i>', '<i class="bi bi-arrow-right"></i>'],
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      992: {
-        items: 2,
-      },
-      1200: {
-        items: 3,
-      },
-    },
-  });
-
-  // testimonial carousel
-  $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    center: true,
-    dots: true,
+    center: false,
+    dots: false,
     loop: true,
     margin: 25,
     nav: true,
