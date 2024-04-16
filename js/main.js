dfddb19c850fbc1a295a2b84e9833d20cf0c6cf1
dzikri028 <yuandazahraaulia@gmail.com>
2024-04-16 17:50:31 +0700

    aku apa

--- js/main.js
+++ js/main.js
@@ -148,6 +148,31 @@
   });
 
   // Kategori
+  $(".category-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: false,
+    dots: false,
+    loop: true,
+    margin: 25,
+    nav: true,
+    navText: ['<i class="bi bi-arrow-left"></i>', '<i class="bi bi-arrow-right"></i>'],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
 
   // testimonial carousel
   $(".testimonial-carousel").owlCarousel({
