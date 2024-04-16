0fa1ca42e76e57b4422b3767c8eeb6a709645bb9
dzikri028 <yuandazahraaulia@gmail.com>
2024-04-16 21:40:15 +0700

    aku pusing

--- js/main.js
+++ js/main.js
@@ -174,6 +174,33 @@
     },
   });
 
+  // Informasi carousel
+  $(".info-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav: true,
+    navText: ['<i class="bi bi-arrow-left"></i>', '<i class="bi bi-arrow-right"></i>'],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+
   // testimonial carousel
   $(".testimonial-carousel").owlCarousel({
     autoplay: true,
